0b2e51556b1d8e6787652cecefbfa3b12bc5da14
wadhikara <159686488+wadhikara@users.noreply.github.com>
2024-02-11 15:42:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('nav ul li a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('nav ul li a [href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+};
